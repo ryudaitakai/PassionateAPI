aa943187e67096977dbd10796cd1bd1d63a895fd
ryudai.takai <>
2022-11-16 21:37:17 +0900

    Initialize

---
+++ starttoday/src/main/java/com/example/starttoday/StarttodayApplication.java
@@ -0,0 +1,13 @@
+package com.example.starttoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarttodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarttodayApplication.class, args);
+	}
+
+}
---
+++ starttoday/src/test/java/com/example/starttoday/StarttodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starttoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarttodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
