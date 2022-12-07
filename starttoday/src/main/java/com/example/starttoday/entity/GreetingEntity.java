461caf7ff73294007b17c03fd7f211261d9d9c67
ryudai.takai <>
2022-12-07 21:45:33 +0900

    validationを追加

--- starttoday/src/main/java/com/example/starttoday/entity/GreetingEntity.java
+++ starttoday/src/main/java/com/example/starttoday/entity/GreetingEntity.java
@@ -2,10 +2,16 @@ package com.example.starttoday.entity;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class GreetingEntity {
 
     private final long id;
+
+    @NotBlank
+    @Size(min=1,max=256)
     private final String content;
 
 
