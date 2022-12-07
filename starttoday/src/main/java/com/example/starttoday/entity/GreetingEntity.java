57b12ba42d70afca400648a68ff0bf2d1b407649
ryudai.takai <>
2022-12-07 21:17:31 +0900

    lombokセッティング

--- starttoday/src/main/java/com/example/starttoday/entity/GreetingEntity.java
+++ starttoday/src/main/java/com/example/starttoday/entity/GreetingEntity.java
@@ -1,29 +1,12 @@
 package com.example.starttoday.entity;
 
+import lombok.Data;
+
+@Data
 public class GreetingEntity {
 
     private final long id;
     private final String content;
 
-    public GreetingEntity(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
 
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-
-    public long setId(long id) {
-        return id;
-    }
-
-    public String setContent(String content) {
-        return content;
-    }
 }
